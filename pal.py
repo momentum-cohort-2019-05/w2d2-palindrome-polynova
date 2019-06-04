19dc7e839923202a689ea04d417e32f44a2494a5
polynova <jocelyncasanova1@gmail.com>
2019-06-04 14:49:44 -0400

    added the figuiring out if it is a pal

--- pal.py
+++ pal.py
@@ -1,2 +1,11 @@
 user_sentence = input("Give me sentences right now!")
 print(user_sentence)
+
+all_letters = "abcdefghijklmnopqrstuvwxyz"
+found_letters = []
+for letter in sentence.lower():
+    if letter in all_letters and letter not in found_letters:
+        found_letters.append(letter)
+        
+print(found_letters)
+
