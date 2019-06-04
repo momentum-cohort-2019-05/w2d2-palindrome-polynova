840b771802aac5b656e7ac0b002f271779793d5a
polynova <jocelyncasanova1@gmail.com>
2019-06-04 14:52:29 -0400

    defined sentence

--- pal.py
+++ pal.py
@@ -1,11 +1,12 @@
 user_sentence = input("Give me sentences right now!")
 print(user_sentence)
 
+sentence = user_sentence 
 all_letters = "abcdefghijklmnopqrstuvwxyz"
 found_letters = []
 for letter in sentence.lower():
     if letter in all_letters and letter not in found_letters:
         found_letters.append(letter)
-        
+
 print(found_letters)
 
