0968cbff6716957b1ee256a5aac0a32b90d6862c
polynova <jocelyncasanova1@gmail.com>
2019-06-04 18:10:33 -0400

    added clean text and elif

--- pal.py
+++ pal.py
@@ -1,12 +1,33 @@
-user_sentence = input("Give me sentences right now!")
-print(user_sentence)
+sentence = input("Give me sentences right now!")
+print(sentence)
 
-sentence = user_sentence 
-all_letters = "abcdefghijklmnopqrstuvwxyz"
-found_letters = []
-for letter in sentence.lower():
-    if letter in all_letters and letter not in found_letters:
-        found_letters.append(letter)
+# all_letters = "abcdefghijklmnopqrstuvwxyz"
+# found_letters = []
+# for letter in sentence.lower():
+#     if letter in all_letters and letter not in found_letters:
+#         found_letters.append(letter)
 
-print(found_letters)
+# print(found_letters)
+
+def clean_text(text):
+    """
+    Given a text, return the text with no spaces or punctuation and all lowercased.
+    """
+    new_text = ""
+    text = text.lower()
+    for character in text:
+        if character.isalpha():
+            new_text = new_text + character
+    return new_text
+
+cleanned_sentence = clean_text(sentence)
+reverse_sentence = cleanned_sentence[::-1]
+
+if cleanned_sentence == reverse_sentence:
+    print ("It is a palindrome")
+
+elif cleanned_sentence != reverse_sentence:
+    print ("This is not a palindrome")
+
+
 
